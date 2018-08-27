b9e3cf27782f351f7e1ca9405b66dfe524719ac2
SIMTAE-MAC <simone.taeggi@MacBook-Pro-di-SIMTAE-MAC.local>
2018-08-27 21:40:18 +0200

    Native Icons set

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Ionicons } from '@expo/vector-icons';
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Opening up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Ionicons name='ios-pizza' color='red' size={100} />
       </View>
     );
   }
