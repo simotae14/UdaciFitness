0b8cff8676826ef2dd58614dd8ee40b447c5054e
SIMTAE-MAC <simone.taeggi@MacBook-Pro-di-SIMTAE-MAC.local>
2018-08-27 23:54:14 +0200

    Render the correct UI Component for each metrics

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { getMetricMetaInfo } from '../utils/helpers';
+
+import UdaciSlider from './UdaciSlider';
+import UdaciSteppers from './UdaciSteppers';
 
 export default class AddEntry extends Component {
 	state = {
@@ -36,9 +39,33 @@ export default class AddEntry extends Component {
 		}))
 	}
   	render() {
+  		const metaInfo = getMetricMetaInfo();
+
     	return (
       		<View>
-				{ getMetricMetaInfo('bike').getIcon() }
+				{ Object.keys(metaInfo).map((key) => {
+					const { getIcon, type, ...rest } = metaInfo[key];
+					const value = this.state[key]
+
+					return (
+						<View key={key}>
+							{ getIcon() }
+							{ type === 'slider'
+								? <UdaciSlider
+									value={value}
+									onChange={() => this.slide(key, value)}
+									{...rest}
+								  />
+								: <UdaciSteppers
+									value={value}
+									onIncrement={() => this.increment(key)}
+									onDecrement={() => this.decrement(key)}
+									{...rest}
+								  />
+							}
+						</View>
+					);
+				})}
 			</View>
 		);
 	}
