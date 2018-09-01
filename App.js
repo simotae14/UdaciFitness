f1dbfb6b2fb09d785e88a18bee77d9edcbf38884
Simone Taeggi <simonetaeggi@MacBook-Pro-di-Simone.local>
2018-09-01 15:35:53 +0200

    Testing the Slider component

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import { View } from 'react-native';
-import AddEntry from './components/AddEntry';
+import { StyleSheet, Text, View, Slider } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    value: 0
+  }
   render() {
     return (
-      <View>         
-        <AddEntry />
+      <View style={styles.container}>
+        <Slider
+          minimumValue={-10}
+          maximumValue={10}
+          step={1}
+          value={this.state.value}
+          onValueChange={(value) => this.setState(() => ({
+            value
+          }))}
+         />
+         <Text>Value: { this.state.value }</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    alignItems: 'stretch',
+    justifyContent: 'center'
+  }
+})
 
