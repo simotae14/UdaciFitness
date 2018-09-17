48155db337fcb44a42fb3bd82ed3684b8a00d8b9
Simone Taeggi <simone.taeggi@leovegas.com>
2018-09-17 23:45:26 +0200

    Added AppLoading until the data aren't ready

--- components/History.js
+++ components/History.js
@@ -8,7 +8,11 @@ import UdaciFitnessCalendar from 'udacifitness-calendar';
 import { white } from '../utils/colors';
 import DateHeader from './DateHeader';
 import MetricCard from './MetricCard';
+import { AppLoading } from 'expo';
 class History extends Component {
+    state = {
+        ready: false,
+    }
     componentDidMount() {
         const { dispatch } = this.props;
 
@@ -21,7 +25,8 @@ class History extends Component {
                         [timeToString()]: getDailyReminderValue()
                     }));
                 }
-            });
+            })
+            .then(() => this.setState(() => ({ready: true})));
     }
     renderItem = ({ today, ...metrics }, formattedDate, key) => (
         <View style={styles.item}>
@@ -50,6 +55,10 @@ class History extends Component {
     }
     render() {
         const { entries } = this.props;
+        const { ready } = this.state;
+        if (ready === false) {
+            return <AppLoading />
+        }
         return (
             <UdaciFitnessCalendar
                 items={entries}
