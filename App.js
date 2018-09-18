2006f2cde97040d1a61f3c4ed0c01cec16d1af0c
Simone Taeggi <simone.taeggi@leovegas.com>
2018-09-18 17:20:49 +0200

    tabNavigator

--- App.js
+++ App.js
@@ -1,56 +1,58 @@
-/* Demo TabNavigator */
 import React from 'react';
-import {
-  Text,
-  View,
-  StyleSheet
-} from 'react-native';
+import { View, Platform } from 'react-native';
+import AddEntry from './components/AddEntry';
+import History from './components/History';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import { TabNavigator } from 'react-navigation';
+import { purple, white } from './utils/colors';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
-import { TabNavigator, StackNavigator, DrawerNavigator } from 'react-navigation';
-
-function Home() {
-  return (
-    <View style={styles.container}>
-      <Text>HOME</Text>
-    </View>
-  );
-}
-
-function Dashboard() {
-  return (
-    <View style={styles.container}>
-      <Text>Dashboard</Text>
-    </View>
-  );
-}
 
 const Tabs = TabNavigator({
-  Home: {
-    screen: Home,
+  History: {
+    screen: History,
     navigationOptions: {
-      tabBarIcon: () => <FontAwesome name='home' size={30} color='black' />
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
     }
   },
-  Dashboard: {
-    screen: Dashboard,
+  AddEntry: {
+    screen: AddEntry,
     navigationOptions: {
-      tabBarIcon: () => <FontAwesome name='dashboard' size={30} color='black' />
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
     }
   }
 });
-
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Tabs />
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={{ flex: 1 }}>
+          <View style={{ height: 20 }} />
+          <Tabs />
+        </View>
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-});
