71e32928810c4c4cb4b00bdb24e75cdb6012ec1c
Simone Taeggi <simone.taeggi@leovegas.com>
2018-09-18 15:41:07 +0200

    Demo tabNavigation

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
+/* Demo TabNavigator */
 import React from 'react';
-import { View, Slider } from 'react-native';
-import AddEntry from './components/AddEntry';
-import History from './components/History';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from './reducers';
+import {
+  Text,
+  View,
+  StyleSheet
+} from 'react-native';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { TabNavigator, StackNavigator, DrawerNavigator } from 'react-navigation';
+
+function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>HOME</Text>
+    </View>
+  );
+}
+
+function Dashboard() {
+  return (
+    <View style={styles.container}>
+      <Text>Dashboard</Text>
+    </View>
+  );
+}
+
+const Tabs = TabNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarIcon: () => <FontAwesome name='home' size={30} color='black' />
+    }
+  },
+  Dashboard: {
+    screen: Dashboard,
+    navigationOptions: {
+      tabBarIcon: () => <FontAwesome name='dashboard' size={30} color='black' />
+    }
+  }
+});
 
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={createStore(reducer)}>
-        <View style={{ flex: 1 }}>
-          <View style={{ height: 20 }} />
-          <History />
-        </View>
-      </Provider>
+      <View style={styles.container}>
+        <Tabs />
+      </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
